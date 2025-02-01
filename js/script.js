0532447bd45bfe0858ef10ff5f58a37fe7ad4e95
= <=>
2025-02-01 13:28:30 +0800

    selesai menambahkan efek paralax pada halaman web

--- js/script.js
+++ js/script.js
@@ -11,3 +11,40 @@ $(document).ready(function() {
         e.preventDefault();
     });
 });
+
+
+// paralax
+
+// About
+$(window).on('load', function() {
+    $('.kiri').addClass('pMuncul');
+    $('.kanan').addClass('pMuncul');
+});
+
+
+$(window).scroll( function() {
+
+    const wScroll = $(this).scrollTop();
+
+    // jumbotron
+    $('.jumbotron img').css({
+        'transform' : `translate(0px, ${wScroll / 4}%)`
+    })
+    $('.jumbotron h1.nama').css({
+        'transform' : `translate(0px, ${wScroll / 2}%)`
+    })
+    $('.jumbotron h3').css({
+        'transform' : `translate(0px, ${wScroll / 1}%)`
+    })
+
+    // portfolio
+    if( wScroll > $('.portfolio').offset().top - 200) {
+        $('.portfolio img').each( function(i) {
+            setTimeout(function() {
+                $('.portfolio img').eq(i).addClass('muncul');
+            }, 300 * (i+1))
+        });
+
+    }
+
+});
